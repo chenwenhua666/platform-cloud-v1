ffd681a4bac6aa647c238c9c96ddfc349e94cdf9
chenwenhua666 <764471698@qq.com>
2020-04-07 14:18:53 +0800

    参数校验

--- platform-common/src/main/java/com/plm/platform/common/handler/BaseExceptionHandler.java
+++ platform-common/src/main/java/com/plm/platform/common/handler/BaseExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -75,6 +76,24 @@ public class BaseExceptionHandler {
             message.append(pathArr[1]).append(violation.getMessage()).append(",");
         }
         message = new StringBuilder(message.substring(0, message.length() - 1));
+        return new PlatformResponse().message(message.toString());
+    }
+
+    /**
+     * 统一处理请求参数校验(json)
+     *
+     * @param e ConstraintViolationException
+     * @return PlatformResponse
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public PlatformResponse handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        StringBuilder message = new StringBuilder();
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            message.append(error.getField()).append(error.getDefaultMessage()).append(",");
+        }
+        message = new StringBuilder(message.substring(0, message.length() - 1));
+        log.error(message.toString(), e);
         return new PlatformResponse().message(message.toString());
     }
 
